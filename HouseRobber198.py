ee9d869cd93633e8f22d2ee9afa81b8256af32e0
Pshiv13 <Pshiv13798@gmail.com>
2024-05-16 21:42:29 +0530

    Update HouseRobber198.py

--- HouseRobber198.py
+++ HouseRobber198.py
@@ -1,1 +1,14 @@
+## You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night. 
+## Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 
+##  [rob1, rob2, n , n+1, ...]
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # [rob1, rob2, n , n+1, ...]
+        rob1, rob2 = 0, 0
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2

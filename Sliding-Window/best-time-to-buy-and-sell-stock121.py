6da5bdb59c63696b3f44163ac8d082317058b83a
Pshiv13 <Pshiv13798@gmail.com>
2024-05-24 14:52:58 +0530

    Create best-time-to-buy-and-sell-stock121.py

---
+++ Sliding-Window/best-time-to-buy-and-sell-stock121.py
@@ -0,0 +1,34 @@
+''' You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0. '''
+
+## use left and right pointer
+## find the max profit
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r, m = 0, 1, 0
+
+        while(r<len(prices)):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                m = max(m, profit)
+            else:
+                l=r         
+            r+=1
+        return m

cfeb904f1c02e661496221161e7321d9b1febfc0
Pshiv13 <Pshiv13798@gmail.com>
2024-05-26 17:23:32 +0530

    Create time-based-key-value-store981.py

---
+++ Binary-Search/time-based-key-value-store981.py
@@ -0,0 +1,47 @@
+''' Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+Example 1:
+
+Input
+["TimeMap", "set", "get", "get", "set", "get", "get"]
+[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+Output
+[null, null, "bar", "bar", null, "bar2", "bar2"] '''
+
+## Use binary search and store closest value all the time
+
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        if key not in self.store: return ""
+        values = self.store.get(key)
+        l, r = 0, len(values) - 1
+
+        while(l<=r):
+            m = (l+r)//2
+            if values[m][1] <= timestamp: 
+                res = values[m][0]
+                l = m+1
+            else:
+                r = m-1
+        return res
+        
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)

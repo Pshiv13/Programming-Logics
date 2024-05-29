c16eb9627cd0be12b00194876c3ddbe37533f9f7
Pshiv13 <Pshiv13798@gmail.com>
2024-05-29 08:53:19 +0530

    Create add-two-numbers2.py

---
+++ LinkedList/add-two-numbers2.py
@@ -0,0 +1,33 @@
+''' You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807. '''
+
+# Using simple math strick with carry and sum of values
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        cur = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            val = val1 + val2 + carry
+            carry = val // 10
+            val = val % 10
+
+            cur.next = ListNode(val)
+            cur = cur.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next

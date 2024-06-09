3ce88342e60980a57fcea96e66bf2781f7e295d3
Pshiv13 <Pshiv13798@gmail.com>
2024-06-09 16:27:01 +0530

    Create CourseScheduleII210.py

---
+++ Graph/CourseScheduleII210.py
@@ -0,0 +1,51 @@
+''' There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+Example 2:
+
+Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+Example 3:
+
+Input: numCourses = 1, prerequisites = []
+Output: [0] '''
+
+## Using DFS approach and mark nodes visited, unvisited and visiting and detect cycles. If cycles detected then return False
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        order = []
+        g = defaultdict(list)
+        UNVISITED, VISITING, VISITED = 0, 1, 2
+        states = [0]*numCourses
+
+        for i, j in prerequisites:
+            g[i].append(j)
+        
+        def dfs(i):
+            if states[i] == VISITED: return True
+            if states[i] == VISITING: return False
+
+            states[i] = VISITING
+            for nei in g[i]:
+                if not dfs(nei):
+                    return False
+            
+            states[i] = VISITED
+            order.append(i)
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        
+        return order

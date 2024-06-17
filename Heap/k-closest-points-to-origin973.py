a523b42bf5b249f9d5ee236642cfbc1da4a61ab7
Pshiv13 <Pshiv13798@gmail.com>
2024-06-17 08:48:36 +0530

    Create k-closest-points-to-origin973.py

---
+++ Heap/k-closest-points-to-origin973.py
@@ -0,0 +1,32 @@
+''' Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+Example 1:
+
+Input: points = [[1,3],[-2,2]], k = 1
+Output: [[-2,2]]
+Explanation:
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]]. '''
+
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def dist(x, y):
+            return x**2 + y**2
+        
+        h = []
+
+        for x, y in points:
+            d = dist(x,y)
+
+            if len(h) < k:
+                heapq.heappush(h, (-d, x, y))
+            else:
+                heapq.heappushpop(h, (-d, x, y))
+        
+        return [(x,y) for d, x, y in h]

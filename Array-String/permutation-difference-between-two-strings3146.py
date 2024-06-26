f66e059b0336d2184cfb010e6e578c9e9f7bc76f
Pshiv13 <Pshiv13798@gmail.com>
2024-06-26 16:41:57 +0530

    Create permutation-difference-between-two-strings3146.py

---
+++ Array-String/permutation-difference-between-two-strings3146.py
@@ -0,0 +1,15 @@
+''' You are given two strings s and t such that every character occurs at most once in s and t is a permutation of s.
+The permutation difference between s and t is defined as the sum of the absolute difference between the index of the occurrence of each character in s and the index of the occurrence of the same character in t.
+Return the permutation difference between s and t.
+Example 1:
+Input: s = "abc", t = "bac"
+Output: 2'''
+
+class Solution:
+    def findPermutationDifference(self, s: str, t: str) -> int:
+        ans = 0
+
+        for i in range(len(s)):
+            ans += abs(i - t.index(s[i]))
+        
+        return ans

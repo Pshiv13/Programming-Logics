8aee14520463155bc330e9fc931e596fe0d18b1d
Pshiv13 <Pshiv13798@gmail.com>
2024-07-28 15:24:51 +0530

    Create minimum-number-of-steps-to-make-two-strings-anagram1347.py

---
+++ HashMap/minimum-number-of-steps-to-make-two-strings-anagram1347.py
@@ -0,0 +1,29 @@
+''' You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+ 
+
+Example 1:
+
+Input: s = "bab", t = "aba"
+Output: 1
+Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s. '''
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        S = set(list(s))
+        ANS = 0
+
+        for i in S:
+            p = s.count(i)
+            q = t.count(i)
+
+            if q >= p:
+                continue
+            else:
+                ANS += (p-q)
+        
+        return ANS

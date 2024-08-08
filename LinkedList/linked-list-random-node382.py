1443768d8b209c45d80284b268182a7e310e888c
Pshiv13 <Pshiv13798@gmail.com>
2024-08-08 16:40:22 +0530

    Create linked-list-random-node382.py

---
+++ LinkedList/linked-list-random-node382.py
@@ -0,0 +1,16 @@
+''' Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+Implement the Solution class:
+Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.'''
+
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.Nodes = []
+
+        while head:
+            self.Nodes.append(head.val)
+            head = head.next
+
+    def getRandom(self) -> int:
+        return self.Nodes[random.randint(0, len(self.Nodes)-1)]
